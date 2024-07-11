10adc4508cd0e59d42fda0453206c8e461383bf6
Jocelyn-Chen5 <cjl1759648974@outlook.com>
2024-07-11 16:44:10 +0800

    add pictures and info on the about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -3,13 +3,14 @@ import { Flex, Layout } from "antd";
 import Navbar from '../../components/Navbar/nav';
 import './about.css';
 import AboutBackground from './AboutBackground.jpg';
+import CoffeePlus from './CoffeePlus.jpg'
 
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const layoutStyle = {
     //   height: "100vh",
-      width: "100vw",
+    //   width: "100vw",
 };
 
 const headerStyle = {
@@ -22,9 +23,9 @@ const headerStyle = {
 }; 
 
 const contentStyle = {
-    textAlign: "center",
+    // textAlign: "center",
   //   height: "80vh",
-    lineHeight: "80vh",
+    // lineHeight: "80vh",
     color: "#fff",
     backgroundColor: "white",
 };
@@ -37,6 +38,12 @@ const imgStyle = {
     height:"40%",
     
   };
+
+const imgPlusStyle = {
+    display: "block",
+    width:"100%",
+    height:"100%",
+};
 
 
 const About = () => {
@@ -55,13 +62,38 @@ const About = () => {
 
                         />
                         <div className="IntroBlock">
-                            <p>
+                            <h1>
                                 UNLOCK the TASTE of PRESSO
-                            </p>
+                            </h1>
                             <p>
-                                Where Passion Meets Perfection in Every Sip
+                            The story of PRESSO begins with a profound passion for coffee – a relentless pursuit to unlock the true essence of the perfect brew. 
+                            It all started with a dream to elevate the coffee experience, to create a product that would captivate the senses and transport the drinker to a realm of pure indulgence. 
+                            The founders of PRESSO, a team of seasoned coffee experts and enthusiasts, embarked on a meticulous journey of discovery.
+                            They traveled the world, exploring the finest coffee-growing regions, seeking out the most exceptional beans and perfecting the art of roasting. 
+                            Each step of the process was meticulously refined, driven by an unwavering commitment to quality and a desire to redefine the boundaries of what a cup of coffee could be.
                             </p>
                         </div>
+
+                        <div className="CoffeeImage">
+                            <img
+                                alt="CoffeeBackground"
+                                src={CoffeePlus}
+                                style={imgPlusStyle}
+                            />
+
+                        </div>
+
+                        <div className="">
+                            <h1>
+                                "PRESSO - Infusing Life with Unique Allure"
+                            </h1>
+                            <p>
+                                Focused on developing and providing high-quality coffee.
+                                Striving for perfection in every step, from source to cup.
+                                Committed to delivering a distinct and unforgettable coffee experience for consumers.
+                            </p>
+                        </div>
+                    
                     </div>
                     
 
